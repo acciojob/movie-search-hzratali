1a5d80bfae2b36e74ee1ec99061dfb1b4d4fdaf9
Hazrat Ali <hazrat8143@gmail.com>
2024-04-22 23:48:00 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
+import SearchBar from "./SearchBar";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <SearchBar />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
